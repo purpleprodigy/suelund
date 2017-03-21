de467aeed979b4e7d054129eb5684ef08f75a98c
Rose Cox <rose@purpleprodigy.com>
2017-03-21 13:50:21 +1000

    Remove front page gallery roll up text

--- lib/structure/footer.php
+++ lib/structure/footer.php
@@ -20,6 +20,6 @@ add_filter( 'genesis_footer_creds_text', __NAMESPACE__ . '\pp_footer_creds_filte
  * @return string
  */
 function pp_footer_creds_filter( $creds ) {
-	$creds = '<p>Copyright [footer_copyright] <a href="https://suelund.com">Sue Lund - Paint!</a> &middot; <a href="/sitemap/" rel="nofollow">Sitemap</a> &middot; <a href="/privacy/" rel="nofollow">Privacy</a> &middot; Website by <a href="http://www.purpleprodigy.com">Purple Prodigy</a></p>';
+	$creds = '<p>Copyright [footer_copyright] <a href="https://suelund.com">Sue Lund</a> &middot; <a href="/sitemap/" rel="nofollow">Sitemap</a> &middot; <a href="/privacy/" rel="nofollow">Privacy</a> &middot; Website by <a href="http://www.purpleprodigy.com">Purple Prodigy</a></p>';
 	return $creds;
 }
