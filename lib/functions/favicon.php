73bd8dd5928812eb9c54c62edf151f930e1410d8
Rose Cox <rose@purpleprodigy.com>
2017-03-22 11:17:19 +1000

    Fix HTML validation errors

--- lib/functions/favicon.php
+++ lib/functions/favicon.php
@@ -12,14 +12,14 @@ namespace SueLund;
 
 //* Add Favicon
 function pp_favicon() {?>
-	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/apple-touch-icon.png">
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/favicon.ico">
 	<?php
 }
